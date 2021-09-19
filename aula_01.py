1f9296a9ba05644bf25a639522bf91b0f9f2d8d9
professorjar <66971871+professorjar@users.noreply.github.com>
2021-09-19 16:56:05 -0300

    Plataforma bvs
    Plataforma bvs

--- aula_01.py
+++ aula_01.py
@@ -1,5 +1,5 @@
 #Autor: João Tinti
-#
+#Fork : José Alberto Rodrigues 19/09/2021
 import pygame
 from pygame.locals import *
 from sys import exit
@@ -10,7 +10,7 @@ largura = 640
 altura = 480
 
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Jogo')
+pygame.display.set_caption('Curso de Python3 Plataforma BVS')
 
 while True:
     for event in pygame.event.get():
